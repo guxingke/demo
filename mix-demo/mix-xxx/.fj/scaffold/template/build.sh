07fd5a141f288f3873d4166e211e50bdbeedca68
gxk <liang.lu@duitang.com>
2019-12-25 17:05:16 +0800

    .

--- mix-demo/mix-xxx/.fj/scaffold/template/build.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-native-image -cp target/{{name}}.jar -H:Name={{name}} -H:+ReportUnsupportedElementsAtRuntime --no-server {{mainClass}}
--- mix-demo/mix-xxx/build.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-native-image -cp target/mix-xxx.jar \
-  -H:Name=mix-xxx \
-  -H:ReflectionConfigurationFiles=reflections.json \
-  -H:+ReportUnsupportedElementsAtRuntime \
-  --no-server \
-  com.gxk.demo.Main
