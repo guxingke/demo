d8bb1a9e4a527c8d5d838c27a32847f0a896f3b5
guxingke <admin@guxingke.com>
2021-06-11 10:50:42 +0800

    .

--- compile-demo/src/main/java/com/gxk/demo/Main.java
+++ compile-demo/src/main/java/com/gxk/demo/Main.java
@@ -3,11 +3,15 @@ package com.gxk.demo;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.nio.charset.Charset;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Locale;
+import javax.tools.Diagnostic;
 import javax.tools.DiagnosticCollector;
 import javax.tools.JavaCompiler;
 import javax.tools.JavaCompiler.CompilationTask;
+import javax.tools.JavaFileObject;
 import javax.tools.StandardJavaFileManager;
 import javax.tools.StandardLocation;
 import javax.tools.ToolProvider;
@@ -17,8 +21,9 @@ public class Main {
   public static void main(String[] args)
       throws ClassNotFoundException, NoSuchMethodException, InvocationTargetException, IllegalAccessException {
     final JavaCompiler compiler = ToolProvider.getSystemJavaCompiler();
+    final DiagnosticCollector<JavaFileObject> dc = new DiagnosticCollector<>();
     final StandardJavaFileManager manager = compiler
-        .getStandardFileManager(new DiagnosticCollector<>(), Locale.ENGLISH, Charset.defaultCharset());
+        .getStandardFileManager(dc, Locale.ENGLISH, Charset.defaultCharset());
 
     final JdkDynamicCompileClassLoader classLoader = new JdkDynamicCompileClassLoader(
         Thread.currentThread().getContextClassLoader());
@@ -27,20 +32,35 @@ public class Main {
         manager, classLoader);
 
     final CharSequenceJavaFileObject test = new CharSequenceJavaFileObject("Test",
-        "public class Test { public static void v(int x) {System.out.println(\"Hello \" + x);}}");
+        "public class Test { public static void v(int x) { "
+            + "com.gxk.demo.Test.hello(2); "
+            + "com.gxk.demo.Test.hello(java.lang.Math.abs(-12)); "
+            + "} }");
     fileManager.addJavaFileObject(StandardLocation.SOURCE_PATH, "", "Test.java", test);
 
+    final String cp = Main.class.getClassLoader().getResource("").getPath();
+    List<String> options = new ArrayList<>();
+    options.add("-cp");
+    options.add(cp);
+
     final CompilationTask task = compiler
-        .getTask(null, fileManager, new DiagnosticCollector<>(), null, null, Arrays.asList(test));
+        .getTask(null, fileManager, dc, options, null, Arrays.asList(test));
 
     final Boolean result = task.call();
+    if (result) {
+      System.out.println();
+    }
+    if (!result) {
+      for (Diagnostic<? extends JavaFileObject> diagnostic : dc.getDiagnostics()) {
+        System.err.println(diagnostic);
+      }
+      return;
+    }
 
     final Class<?> cls = classLoader.loadClass("Test");
 
     final Method main = cls.getDeclaredMethod("v", int.class);
 
-    main.invoke(null , 1);
-    System.out.println(result);
-
+    main.invoke(null, 1);
   }
 }
---
+++ compile-demo/src/main/java/com/gxk/demo/Test.java
@@ -0,0 +1,8 @@
+package com.gxk.demo;
+
+public class Test {
+
+  public static void hello(int x) {
+    System.out.println("hello " + x);
+  }
+}
